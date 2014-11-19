c366653b015d393cfb747bdedfd206a98dc3e56b
Scombster <kofod88@gmail.com>
2014-11-19 16:29:53 +0100

    Animations and Subtitle script change
    Pictures, seagull animations.

--- IslandGame/Assets/Scripts/OpeningSequence.cs
+++ IslandGame/Assets/Scripts/OpeningSequence.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class OpeningSequence : MonoBehaviour {
 
+	private float DistanceX;
+	private float DistanceZ;
+
 	//Name this variable "ShowText" then drag the "ShowText" script into the variable through the inspector.
 	public ShowText textDisplayingScript;
 
-	void Start () {
-		//Shows two lines of text at the bottom left of the screen (10 pixels from the left and 50 from the bottom).
-		//The text lasts for 5 seconds before it disappears.
-		textDisplayingScript.DisplayTextHereFor ("There is enough room for the food in my back. \n" +
-		                                         "I will put it there.", 10,  50,  5);
+	void Update () {
 
-		//Subtitles can be added with triggers/timers and/or if statements
+		DistanceX = Mathf.Abs (transform.position.x) - Mathf.Abs (Movement2.GetPlayerX());
+		DistanceZ = Mathf.Abs (transform.position.z) - Mathf.Abs (Movement2.GetPlayerZ());
+		
+		if (DistanceX <= 4 && DistanceZ <= 4 && DistanceX >= -4 && DistanceZ >= -4) {
+						//Shows two lines of text at the bottom left of the screen (10 pixels from the left and 50 from the bottom).
+						//The text lasts for 5 seconds before it disappears.
+						textDisplayingScript.DisplayTextHereFor ("Not gonna feed the sharks today..", 10, 50, 3);
+		}
+
 	}
 }
