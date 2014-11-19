b25ce546b1bc001521b1f64618d62e666256c922
kyhnmette <kyhnmette@gmail.com>
2014-11-19 12:23:22 +0100

    Merge branch 'master' of https://github.com/LisbethRasmussen/IslandAdventure

---
+++ IslandGame/Assets/Scripts/OpeningSequence.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningSequence : MonoBehaviour {
+
+	//Name this variable "ShowText" then drag the "ShowText" script into the variable through the inspector.
+	public ShowText textDisplayingScript;
+
+	void Start () {
+		//Shows two lines of text at the bottom left of the screen (10 pixels from the left and 50 from the bottom).
+		//The text lasts for 5 seconds before it disappears.
+		textDisplayingScript.DisplayTextHereFor ("There is enough room for the food in my back. \n" +
+		                                         "I will put it there.", 10,  50,  5);
+
+		//Subtitles can be added with triggers/timers and/or if statements
+	}
+}
