c281eaf6a934d8e5c6b23ccd7d6ca07103040bd6
Scombster <kofod88@gmail.com>
2014-11-20 14:33:48 +0100

    New subtitle script for colliders

---
+++ IslandGame/Assets/Scripts/Subtitles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Subtitles : MonoBehaviour {
+
+	//Name this variable "ShowText" then drag the "ShowText" script into the variable through the inspector.
+	public ShowText textDisplayingScript;
+
+	void OnTriggerEnter(Collision other){
+
+		textDisplayingScript.DisplayTextHereFor ("WORKTZZZ", 10, 50, 5);
+
+
+	}
+}
