47f23dad83b4eac5142a93bb3c62fe9b0fdbfe22
Scombster <kofod88@gmail.com>
2014-11-20 16:14:55 +0100

    Shark animations (Bait) + Scripts
    *6 shark routes
    *2 scripts to make the sharks follow the "bait"

--- IslandGame/Assets/Scripts/Shark.cs
+++ IslandGame/Assets/Scripts/Shark.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class Shark : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+	private float sharkDistanceX; //distance from bait to shark
+	private float safeRange = 1.0f;
+	private static float sharkPosX;
+
 	void Update () {
+		sharkPosX = transform.position.x;
+
+		sharkDistanceX = Mathf.Abs(sharkPosX - SharkBait.GetBaitX());
+
 		transform.LookAt (GameObject.Find ("SharkBox").transform);	// Makes the Shark look towards the object named "SharkBox"
+
+		if (sharkDistanceX >= safeRange){
+			transform.position += transform.forward * 10 * Time.deltaTime;
+		}
+
 	}
 }
