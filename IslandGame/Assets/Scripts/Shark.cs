55f67bd141bc5c6ee571d22569ecbd91c3aa30a0
Steffen Pihl <spihl12@student.aau.dk>
2014-11-20 15:12:34 +0100

    The script for the shark to look at a box :fish:
    - Assign the "Shark" script to the shark
    - Make a empty object called "SharkBox" (if called something else shall
    the code be changed to the right name)
    
    The shark will then ALWAYS look at the box's position :fish:

---
+++ IslandGame/Assets/Scripts/Shark.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shark : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (GameObject.Find ("SharkBox").transform);	// Makes the Shark look towards the object named "SharkBox"
+	}
+}
