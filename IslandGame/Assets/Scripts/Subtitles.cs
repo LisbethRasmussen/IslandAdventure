9a9fb47d45a4a50e34a94370cf3dedecf88b08c8
Steffen Pihl <spihl12@student.aau.dk>
2014-11-23 17:46:07 +0100

    Subtitles added (not all) READ DISCRIPTION!!!
    Many things happens in this update so here is the list:
    - I removed the skybox! Otherwise couldn't I work so someone have to set
    it back when we need it (or maybe just use a nicer default skybox?)
    - Subtittles have been added this way:
    -- Set an empty object (I called them "Subtitle##")
    -- Set what text they should display and after it triggers when it shall
    play (default is 0 which is right away)
    -- Go into "myStyle" and set fontSize to 24 and Allignment to "Middle
    Center"
    
    Comments so far:
    I have been using the story doc to find out where the subtitles should
    be and I am confused about a few, I added comments in the word dokument
    so look there!
    The object called "Subtitle03 (Cave, after getting food)" is deactive as
    when he returns after getting food shall he say something, so this shall
    be activated again when he have got all the food. Also the timers in
    that object might need to be fixed at some point. After ALL the
    subtittles are done for this object (around 40 seconds of waitting)
    shall the choice 3 trigger... not sure how that should be done tho...
    
    The object called "Subtitle04 (Cave, choice 3A)" is also deactivated and
    shall be activated if the player decides to stay in the cave (3A). When
    this is done shall Carl appear and dialogue 8 shall start.
    
    After "Subtitle 07 (Dying man)" is done (15 sec) then shall the dialogue
    13 trigger (again have no clue how this shall be done, maybe a whole new
    script with the same kind of Invoke as the subtitle script)
    
    I got to right under dialogue 14 (where they see the boat) and "died
    mentally" after that... so I will stop for now, PLEASE read my comments
    in the story doc and we can talk about them tomorrow!

--- IslandGame/Assets/Scripts/Subtitles.cs
+++ IslandGame/Assets/Scripts/Subtitles.cs
@@ -9,17 +9,16 @@ public class Subtitles : MonoBehaviour {
 	private bool SubTrigger = true;
 
 	public string text;
+	public int StartLine;
 	public GUIStyle myStyle;	//This text style can can be easily changed in the inspector
 
 	void Update () {
 		DistanceX = Mathf.Abs (transform.position.x - Movement2.GetPlayerX ());
 		DistanceZ = Mathf.Abs (transform.position.z - Movement2.GetPlayerZ ());
 	
-		if (DistanceX <= 4 && DistanceZ <= 4 && SubTrigger == true) {
-			//The text lasts for 5 seconds before it disappears.
-			displaying = true;
-			SubTrigger = false; // Makes the subtitle only show up once
-			Invoke ("StopDisplaying", 5);
+		if (DistanceX <= 4 && DistanceZ <= 4 && SubTrigger && !TestStart.GetStartSettings()) {
+			SubTrigger = false;
+			Invoke ("StartDisplaying", StartLine);
 		}
 	
 	}
@@ -27,9 +26,14 @@ public class Subtitles : MonoBehaviour {
 	//Draws the text on the GUI while "displaying" is true
 	void OnGUI() {
 		if (displaying) {
-			GUI.Label(new Rect(10, Screen.height - 100, 0, 0), text, myStyle);
+			GUI.Label(new Rect(Screen.width/2, Screen.height - 100, 0, 0), text, myStyle);
 		}
 		
+	}
+
+	void StartDisplaying () {
+		displaying = true;
+		Invoke ("StopDisplaying", 5);
 	}
 
 	//Simply says to stop displaying
