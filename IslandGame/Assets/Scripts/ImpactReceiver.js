0bde4fd90cec0d901983c1bd3044a594291294bc
Scombster <kofod88@gmail.com>
2014-11-28 13:28:35 +0100

    Pushing

---
+++ IslandGame/Assets/Scripts/ImpactReceiver.js
@@ -0,0 +1,19 @@
+﻿
+var mass = 3.0; // defines the character mass
+var impact = Vector3.zero; // momentum
+private var character: CharacterController;
+
+function Start(){
+	character = GetComponent(CharacterController);
+}
+// call this function to add an impact force:
+function AddImpact(force: Vector3){
+	if (force.y < 0) force.y = -force.y; // reflect down force on the ground
+		impact += force / mass;
+}
+
+function Update(){
+// apply the impact force:
+	if (impact.magnitude > 0.2) character.Move(impact * Time.deltaTime);
+		impact = Vector3.Lerp(impact, Vector3.zero, 5*Time.deltaTime); // consumes the impact energy each cycle:
+}
